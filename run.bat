33be1b78e7e159dfed3be3a3646ecf243a718bbe
Willem Van Zwol <willem@vanzwol.com>
2025-10-29 22:21:53 -0700

    Created installer script for windows
    There is a NSIS script which creates a installer executable in the
    build/ directory, upon running puts the tinytcp.exe file into "Program
    Files\" under "tinytcp" and adds it to PATH such that you can run
    `tinytcp` in the terminal now. And running two in the same terminal
    doesn't result in issues.

--- run.bat
+++ run.bat
@@ -1,15 +1,18 @@
 @echo off
+REM Create build folder if it doesn't exist
 mkdir build 2>nul
+
+REM Remove old binary
 del build\tinytcp.exe 2>nul
 
-rem Collect all .cpp files into a variable
+REM Collect all .cpp files in source/
 setlocal enabledelayedexpansion
 set sources=
 for /r source %%f in (*.cpp) do set sources=!sources! %%f
 
-rem Compile
-c++ %sources% -Iheaders -o build\tinytcp.exe -lws2_32
+REM Compile all .cpp files, include headers, static link
+c++ !sources! -Iheaders -std=c++17 -O2 -g -static -static-libgcc -static-libstdc++ -o build\tinytcp.exe -lws2_32
 
-rem Run
+REM Run the executable with any arguments passed
 build\tinytcp.exe %*
 
